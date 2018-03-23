4807722b0c92fb01ed65e4f9762099936f4865aa
Zoe Kroontje-Gillette <z.gillette@hotmail.com>
2018-03-23 11:58:43 +0100

    Adds needed effects and general soundplayer

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -10,17 +10,12 @@ public class SoundController : MonoBehaviour {
 	void Start () {
 		audioSource = GetComponent<AudioSource> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	/**
 	 * Takes a clip and plays it with through the general audiosource.
 	 * Also takes an option volume param which should be between 0.0 and 1.0
 	 **/
-	private void PlaySound(AudioClip clipToPlay, float volume = 1f)
+	public void PlaySound(AudioClip clipToPlay, float volume = 1f)
 	{
 		audioSource.volume = volume;
 		audioSource.PlayOneShot(clipToPlay);
